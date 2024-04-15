c230d5a7020deffedd67bd78e179ccfde30f7781
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-04-15 20:43:35 +0200

    Show products list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
     public function index(){
-        return Inertia::render("Product");
+        $products = Product::all();
+        return Inertia::render("Product", compact('products'));
     }
 }
--- resources/js/Pages/Product.vue
+++ resources/js/Pages/Product.vue
@@ -1,7 +1,43 @@
 <script setup>
 import AuthLayout from "../Layouts/AuthLayout.vue";
+
+const props = defineProps({
+    products: { type: Array, required: true },
+});
 </script>
 
 <template>
-    <AuthLayout>Produkty boże</AuthLayout>
+    <AuthLayout>
+        <div class="products">
+            <div class="product" v-for="product in products">
+                {{ product.name }}
+                <div class="right">
+                    {{ product.price }}
+
+                    <button>Add to cart</button>
+                </div>
+            </div>
+        </div>
+    </AuthLayout>
 </template>
+
+<style scoped>
+.products {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 2rem;
+}
+.product {
+    border: 1px solid black;
+    padding: 1rem 2rem;
+    width: 650px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.right {
+    display: flex;
+    gap: 1rem;
+}
+</style>
