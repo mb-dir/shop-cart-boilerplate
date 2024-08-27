f0f87aff17e100a96187fc73ea3a694b37929b27
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-08-27 23:57:19 +0200

    Playing with mailtrap

--- app/Mail/OrderEmail.php
+++ app/Mail/OrderEmail.php
@@ -2,51 +2,52 @@
 
 namespace App\Mail;
 
-use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Arr;
-use MailerSend\Helpers\Builder\Variable;
-use MailerSend\Helpers\Builder\Personalization;
-use MailerSend\LaravelDriver\MailerSendTrait;
 
 class OrderEmail extends Mailable
 {
-    use Queueable, SerializesModels, MailerSendTrait;
+    use Queueable, SerializesModels;
 
-    public function build()
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
     {
-        // Recipient for use with variables and/or personalization
-        $to = Arr::get($this->to, '0.address');
+        //
+    }
 
-        return $this
-            ->view('emails.order-confirm')
-            // Additional options for MailerSend API features
-            ->mailersend(
-                template_id: null,
-                variables: [
-                    new Variable($to, ['name' => 'Your Name'])
-                ],
-                tags: ['tag'],
-                personalization: [
-                    new Personalization($to, [
-                        'var' => 'variable',
-                        'number' => 123,
-                        'object' => [
-                            'key' => 'object-value'
-                        ],
-                        'objectCollection' => [
-                            [
-                                'name' => 'John'
-                            ],
-                            [
-                                'name' => 'Patrick'
-                            ]
-                        ],
-                    ])
-                ],
-                precedenceBulkHeader: true,
-            );
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Order Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.order-confirm',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
     }
 }
