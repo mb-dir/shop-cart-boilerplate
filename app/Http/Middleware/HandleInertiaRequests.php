a4b9eb7ffbd1f026adc9b3086ebe5eea1e4fc7f6
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-09-02 16:44:26 +0200

    Pass currencies data as props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Facades\Cart;
+use App\Models\Currency;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -30,6 +31,9 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $currencies = Currency::all();
+        $activeCurrency = $currencies->firstWhere('is_active', 1);
+
         return [
             ...parent::share($request),
             'auth' => [
@@ -39,6 +43,8 @@ class HandleInertiaRequests extends Middleware
                 'message' => fn() => $request->session()->get('message')
             ],
             'cart' => Cart::shared(),
+            'currencies' => $currencies,
+            'activeCurrency' => $activeCurrency,
         ];
     }
 }
